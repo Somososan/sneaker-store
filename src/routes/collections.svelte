eef09bc2a3a84d7c7957e851e5096070ba9ee6c8
C Karaliolios <ckaraliolios@gmail.com>
2021-12-13 21:53:33 +0100

    correction

---
+++ src/routes/collections.svelte
@@ -0,0 +1,62 @@
+<section>
+	<div class="introduction">
+		<h1>Get inspired!</h1>
+		<h3>By our wonderful seasonal or year round collections</h3>
+	</div>
+
+	<div>
+		<img src="" alt="" />
+		<h1>Summer</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente est impedit molestiae
+			voluptates possimus voluptas veniam. Atque molestiae tempore itaque repellat quibusdam error
+			odio, aliquam quas veritatis animi ullam ea totam optio expedita maiores consectetur aperiam
+			amet sed placeat cupiditate perferendis! Accusantium ad voluptatem nemo, vitae ipsam numquam,
+			repellat ea, blanditiis sed non autem. Optio, dicta deserunt provident dolor incidunt velit
+			illo maiores esse cumque ea distinctio eveniet, neque nostrum exercitationem tenetur corrupti
+			libero aperiam!
+		</p>
+		<a href="collections/summer">Show</a>
+	</div>
+	<div>
+		<img src="" alt="" />
+		<h1>Autumn</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, ex officia earum iusto
+			similique fuga nostrum vero cum recusandae totam fugit autem, magnam optio harum! Accusantium
+			odio dolore aperiam neque harum, fuga optio quos, blanditiis error numquam porro fugiat
+			impedit.
+		</p>
+		<a href="collections/autamn">Show</a>
+	</div>
+	<div>
+		<img src="" alt="" />
+		<h1>Winter</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum neque magni maiores
+			molestiae reprehenderit at nesciunt accusamus nobis! Enim dignissimos fugiat sit! Debitis quos
+			quasi, animi recusandae adipisci fuga maxime hic et ab illo provident, ducimus similique,
+			eaque deserunt deleniti vitae corporis voluptate aut. Eaque deleniti asperiores id, veniam
+			commodi molestiae excepturi possimus. Id alias, odit laborum iste ab distinctio.
+		</p>
+		<a href="collections/winter">Show</a>
+	</div>
+	<div>
+		<img src="" alt="" />
+		<h1>Spring</h1>
+		<p></p>
+		<a href="collections/spring">Show</a>
+	</div>
+	<div>
+		<img src="" alt="" />
+		<h1>Classic</h1>
+		<p />
+		<a href="collections/classic">Show</a>
+	</div>
+	<div>
+		<img src="" alt="" />
+		<h1>Creator specific</h1>
+		<p />
+		<a href="collections/creator">Show</a>
+	</div>
+</section>
